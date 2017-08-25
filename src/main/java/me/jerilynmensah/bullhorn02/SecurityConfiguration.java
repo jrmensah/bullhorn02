ca4278cfe52113b5cb615ff2d7badc5dcef90544
jrmensah <jmensah4@student.umuc.edu>
2017-08-25 13:45:47 -0400

    Added manual user accounts security

--- src/main/java/me/jerilynmensah/bullhorn02/SecurityConfiguration.java
+++ src/main/java/me/jerilynmensah/bullhorn02/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package me.jerilynmensah.bullhorn02;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,5 +19,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .httpBasic();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+        throws Exception{
+
+        auth.inMemoryAuthentication().
+                withUser("user").password("password").roles("USER");
     }
 }
